7d73eb39fb5498d6ff119e241ddaa09c93e08b52
Tammie Turpen <tammie.turpen@bellevuecollege.edu>
2020-10-30 17:54:35 -0700

    Add new WOD Angular client project

--- WoDAngularClient/README.md
+++ WoDAngularClient/README.md
@@ -1,4 +1,4 @@
-# AngularClientProjectCSS
+# WoDAngularClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- WoDAngularClient/src/app/app.component.spec.ts
+++ WoDAngularClient/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularClientProjectCSS'`, () => {
+  it(`should have as title 'WoDAngularClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularClientProjectCSS');
+    expect(app.title).toEqual('WoDAngularClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularClientProjectCSS app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('WoDAngularClient app is running!');
   });
 });
