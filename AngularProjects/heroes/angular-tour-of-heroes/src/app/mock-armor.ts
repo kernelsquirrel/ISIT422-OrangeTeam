e60643ba6b929b03cbeeaff7c0719cbf5fb8a3dc
Sean Bruce <seanbruce@hotmail.com>
2020-11-02 18:42:10 -0800

    armor service

--- AngularProjects/heroes/angular-tour-of-heroes/src/app/mock-armor.ts
+++ AngularProjects/heroes/angular-tour-of-heroes/src/app/mock-armor.ts
@@ -1,18 +1,18 @@
 import {Armor} from './armordet';
 
 export const ARMORS: Armor[] = [
-  { id: 111, name: 'Reinforce clothing', rating :1/0,strength :1,defense:0,speed:0 },
+  { id: 111, name: 'Reinforce clothingXX', rating :1/0,strength :1,defense:0,speed:0 },
 
- {id: 112, name: 'Kevlar vest', rating :1/3,strength :1,defense:0,speed:0 },
+ {id: 112, name: 'Kevlar vestXX', rating :1/3,strength :1,defense:0,speed:0 },
 
- { id: 113, name: 'Flak jacket', rating :2/4,strength :1,defense:-1,speed:0 },
+ { id: 113, name: 'Flak jacketXX', rating :2/4,strength :1,defense:-1,speed:0 },
 
- { id: 114, name: 'Full Riot Gear', rating :3/5,strength :2,defense:-2,speed:-1},
+ { id: 114, name: 'Full Riot GearXX', rating :3/5,strength :2,defense:-2,speed:-1},
 
- { id: 115, name: 'Leather (hard)', rating :2/0,strength :2,defense:-1,speed:0 },
+ { id: 115, name: 'Leather (hard)XX', rating :2/0,strength :2,defense:-1,speed:0 },
 
- { id: 116, name: 'Chainmail', rating :3/1,strength :3,defense:-2,speed:-2 },
+ { id: 116, name: 'ChainmailXX', rating :3/1,strength :3,defense:-2,speed:-2 },
 
- { id: 117, name: 'Plate', rating :4/2,strength :3,defense:-2,speed:-3 }
+ { id: 117, name: 'PlateXX', rating :4/2,strength :3,defense:-2,speed:-3 }
  
 ];
